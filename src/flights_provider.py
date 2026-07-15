7d8706353d0d332a64d1313889d1c58b2c09a997
larrocaramon-star <larrocaramon@gmail.com>
2026-07-15 11:26:26 -0300

    Update flights_provider.py

--- src/flights_provider.py
+++ src/flights_provider.py
@@ -1,10 +1,33 @@
 # flights_provider.py
-# Conexión con API de vuelos (ejemplo básico)
+import os
+import requests
 
 def get_prices():
-    # Aquí luego conectaremos con la API real
+    api_key = os.environ.get("SKYSCANNER_API_KEY")
+    url = "https://sky-scrapper.p.rapidapi.com/api/v1/flights/searchFlights"
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": "sky-scrapper.p.rapidapi.com"
+    }
+
+    # Ejemplo: Montevideo → Nueva York
+    params = {
+        "originSkyId": "MVD",
+        "destinationSkyId": "JFK",
+        "departureDate": "2027-02-01",
+        "adults": "3",
+        "currency": "USD"
+    }
+
+    response = requests.get(url, headers=headers, params=params).json()
+    price1 = float(response["data"][0]["price"]["amount"])
+
+    # Por ahora dejamos valores de prueba para los otros tramos
+    price2 = 180
+    price3 = 650
+
     return {
-        "MVD-JFK": 720,
-        "JFK-MIA": 180,
-        "MIA-MVD": 650
+        "MVD-JFK": price1,
+        "JFK-MIA": price2,
+        "MIA-MVD": price3
     }
