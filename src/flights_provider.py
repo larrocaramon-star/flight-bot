6f6d7f0a3373952d286819b21df97a39c2ea2937
larrocaramon-star <larrocaramon@gmail.com>
2026-07-15 11:07:14 -0300

    Create flights_provider.py

---
+++ src/flights_provider.py
@@ -0,0 +1,10 @@
+# flights_provider.py
+# Conexión con API de vuelos (ejemplo básico)
+
+def get_prices():
+    # Aquí luego conectaremos con la API real
+    return {
+        "MVD-JFK": 720,
+        "JFK-MIA": 180,
+        "MIA-MVD": 650
+    }
