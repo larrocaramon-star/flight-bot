d51b2f2b03fec14c339d076bc14968ac6bfe67eb
larrocaramon-star <larrocaramon@gmail.com>
2026-07-15 11:59:27 -0300

    Update analysis.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,9 +1,14 @@
-# analysis.py
 def analyze_prices(prices: dict) -> str:
-    total = prices["TOTAL"]["price"]
+    total = (
+        prices["MVD-JFK"]["price"] +
+        prices["JFK-MIA"]["price"] +
+        prices["MIA-MVD"]["price"]
+    )
     report = (
         f"✈ Informe Diario\n"
-        f"Precio total: {total} USD\n"
-        f"Link: {prices['TOTAL']['link']}\n"
+        f"Montevideo → Nueva York: {prices['MVD-JFK']['price']} USD\n{prices['MVD-JFK']['link']}\n\n"
+        f"Nueva York → Miami: {prices['JFK-MIA']['price']} USD\n{prices['JFK-MIA']['link']}\n\n"
+        f"Miami → Montevideo: {prices['MIA-MVD']['price']} USD\n{prices['MIA-MVD']['link']}\n\n"
+        f"💰 Total: {total} USD\n"
     )
     return report
