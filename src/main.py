b1dec55f6af3cc4776963535c334810055218f77
larrocaramon-star <larrocaramon@gmail.com>
2026-07-15 14:59:49 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,19 @@
+# main.py
 import os
 import requests
+from flights_provider import get_prices
+from analysis import analyze_prices
 
 def main():
-    api_key = os.environ.get("KIWI_API_KEY")
-    print("API KEY:", api_key)  # 👈 Verifica que no sea None
+    prices = get_prices()
+    report = analyze_prices(prices)
 
-    url = "https://api.tequila.kiwi.com/v2/search"
-    headers = {"apikey": api_key}
+    token = os.environ.get("TELEGRAM_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-    params = {
-        "fly_from": "MVD",
-        "fly_to": "NYC",
-        "date_from": "01/08/2026",
-        "date_to": "01/08/2026",
-        "adults": 1,
-        "curr": "USD"
-    }
-
-    response = requests.get(url, headers=headers, params=params).json()
-    print("Response:", response)
+    payload = {"chat_id": chat_id, "text": report}
+    requests.post(url, data=payload)
 
 if __name__ == "__main__":
     main()
